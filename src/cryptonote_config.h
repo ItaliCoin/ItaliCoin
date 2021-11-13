45f3746a268e7ae571bcce5ddf74fc415f9e56a7
unknown <fedemar568@gmail.com>
2021-11-13 18:37:26 +0100

    modified:   src/cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -239,7 +239,7 @@ namespace config
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 28082;
     boost::uuids::uuid const NETWORK_ID = {{0x1b, 0x5e, 0x91, 0x45, 0x81, 0x48, 0xfb, 0xfb, 0x21, 0x12, 0x82, 0x8b, 0x55, 0x61, 0x91, 0xc6}};
     std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
-    uint32_t const GENESIS_NONCE = 10001;
+    uint32_t const GENESIS_NONCE = 19024;
   }
 
   namespace stagenet
